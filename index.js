6b2ba4e326f62a9d811ac4456f363ea692819a0d
Fatima Aruna <aruna.fatima@gmail.com>
2023-11-07 18:19:50 +0000

    feat: edit user feat: find user by id feat: delete user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { userCollection } from './configs/db.js';
+import { ObjectId } from 'mongodb';
 
 // Express
 const app = express();
@@ -34,6 +35,7 @@ app.get('/', (request, response) => {
 // Status code 201
 app.post('/users', async (request, response, next) => {
   try {
+    console.log(request.body)
     const { firstname, lastname, email, dob, bio } = request.body;
 
     // Validation
@@ -73,20 +75,67 @@ app.get('/users', async (request, response, next) => {
  *  Returns a single user if found by it id, else it throws an error
  */
 // Status code 200
-app.get('/users/:id', () => {});
+app.get('/users/:id', async (request, response, next) => {
+  try {
+    const userId = request.params.id;
+    // "_id" : ObjectId("5d71522dc452f78e335d2d8b")
+    const user = await userCollection.find({"_id": ObjectId(userId)}).toArray();
+    response.status(200).json(user);
+  } catch(error) {
+    next(error)
+  }
+});
 
 // Status code 200
 // FIelds to be updated are within the approved limit. Check FIG 1.0 for approved fields
 // Updates a user by it id and returns a message telling us if is successful or not
 // Throw an error if request was unsuccessful
-app.patch('/users/:id', () => {});
+app.patch('/users/:id', async (request, response, next) => {
+  try {
+    const userId = request.params.id;
+    let newInformation = {};
+    const { firstname, lastname, email, dob, bio } = request.body;
+
+    if (firstname) newInformation.firstname = firstname;
+    if (lastname)  newInformation.lastname = lastname;
+    if (email) newInformation.email = email;
+    if (dob) newInformation.dob = dob;
+    if (bio) newInformation.bio = bio;
+
+    const patchedUser = await userCollection.updateOne(
+      {"_id": ObjectId(userId)}, // Filter parameter
+      {$set: newInformation} 
+    )
+
+    response.status(200).json(patchedUser)
+
+  } catch(error) {
+    next(error)
+  }
+});
 
 // Status 204
 // Delete a user by it id if found, else throw an error
-app.delete('/users/:id', () => {});
+app.delete('/users/:id', async( request, response, next) => {
+  try {
+    const userId = request.params.id;
+    const deleteResult = await userCollection.deleteOne({"_id": ObjectId(userId)})
+    response.staus(204).json({message: "User deleted"})
+  } catch(error) {
+    next(error)
+  }
+});
 
 // Delete all users, else throw an error
-app.delete('/users', () => {});
+app.delete('/users', async( request, response, next) => {
+  try {
+    const userId = request.params.id;
+    const deleteResult = await userCollection.deleteMany();
+    response.staus(200).json({message: "All users deleted"})
+  } catch(error) {
+    next(error)
+  }
+});
 
 // Central error processing middleware
 app.use((error, request, response, next) => {
